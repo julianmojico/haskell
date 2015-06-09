80a7ccc0827ed1766007f7243416f34fcd57b478
cepix <jmojico@gmail.com>
2015-06-09 20:49:40 -0300

    Update haskell nuevo.hs

--- haskell nuevo.hs
+++ haskell nuevo.hs
@@ -1,3 +1,51 @@
+--practica1
+
+doubleMe x = x + x
+signo :: Integer -> Int
+signo 0 = 0
+signo x = if x>0 then 1 else -1
+
+--import Text.Regex.Posix ((=~))
+
+signo :: Int -> Int
+signo 0 = 0
+signo x = if x>0 then 1 else -1
+
+negativo :: Int -> Bool
+negativo x = signo x == -1
+
+max :: Int -> Int -> Int
+max x y = if x>y then x else y
+
+max3 :: Int -> Int -> Int -> Int
+max3 x y z = Main.max (Main.max x y) z	  --hay alguna manera mejor de escribirlo?
+
+minim x y = if Main.max x y == x then y else x 
+
+-- factorial x = if x==0 then 1 else x*factorial x-1 --entro en loop infinito y me tiro out of memory
+
+
+factorial x = if x==0 then 1 else factorial (x-1)*x
+
+--o de otra manera...
+
+factorial2 0 = 1
+factorial2 n = n*factorial (n-1)
+
+combinatorio x y = if x>y then factorial x `div` factorial y*factorial (x-y) else 0 --como se puede optimizar?
+
+fibonacci 0 = 1
+fibonacci 1 = 1
+fibonacci x =  fibonacci (x-1) + fibonacci (x-2)
+
+--divisiblePor :: Int -> Int -> Bool
+
+div2 :: (Integral a, Fractional a) => a -> a -> a
+div2 x y = x `div` y
+
+divisiblePor :: (Ord a) => a -> a -> Bool			--como extender los tipos que acepta una funcion?
+divisiblePor x y = (x `div` y) < (x / y)
+
 --practica2
 
 data Poste = Src | Dst | Aux deriving (Show)
@@ -22,6 +70,8 @@ data TipoDeSangre =  TipoSangre String Factor deriving (Eq)
 puedeDonar::TipoDeSangre->TipoDeSangre->Bool
 puedeDonar  (TipoSangre (String x) _) (TipoSangre (String y) _) = (x==y)
 puedeDonar (TipoSangre "O" _) (TipoSangre z m) = True
+
+
 
 
  
